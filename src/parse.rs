ff2fe28e864c675b061d11cf00a7a4259340b446
Ben Mather <bwhmather@bwhmather.com>
2014-08-14 21:03:04 +0100

    implement a next token function with a sensible interface
    squeezing it into the iterator trait was not working

--- src/parse.rs
+++ src/parse.rs
@@ -7,6 +7,7 @@ pub enum Token<'src> {
     QuasiQuote,
     Unquote,
     Unpack,
+    EOF,
 }
 
 #[deriving(Clone, PartialEq, Eq, Show)]
@@ -17,26 +18,6 @@ pub type TokenizerResult<'src> = Result<Token<'src>, TokenizerError>;
 pub struct Tokenizer<'src> {
     input: &'src str,
     cursor: uint,
-}
-
-impl<'src> Tokenizer<'src> {
-    fn peek_char(&self) -> Option<char> {
-        if self.cursor >= self.input.len() {
-            return None
-        } else {
-            return Some(self.input.char_at(self.cursor));
-        }
-    }
-
-    fn pop_char(&mut self) -> Option<char> {
-        if self.cursor >= self.input.len() {
-            return None
-        } else {
-            let range = self.input.char_range_at(self.cursor);
-            self.cursor = range.next;
-            return Some(range.ch);
-        }
-    }
 }
 
 fn is_symbol_body(c: Option<char>) -> bool {
@@ -57,22 +38,39 @@ fn is_whitespace(c: Option<char>) -> bool {
     };
 }
 
+impl<'src> Tokenizer<'src> {
+    fn peek_char(&self) -> Option<char> {
+        if self.cursor >= self.input.len() {
+            return None
+        } else {
+            return Some(self.input.char_at(self.cursor));
+        }
+    }
 
-impl<'src> Iterator<TokenizerResult<'src>> for Tokenizer<'src> {
-    fn next(&mut self) -> Option<TokenizerResult<'src>> {
+    fn pop_char(&mut self) -> Option<char> {
+        if self.cursor >= self.input.len() {
+            return None
+        } else {
+            let range = self.input.char_range_at(self.cursor);
+            self.cursor = range.next;
+            return Some(range.ch);
+        }
+    }
+
+    fn next_token(&mut self) -> TokenizerResult<'src> {
         while is_whitespace(self.peek_char()) {
             self.pop_char();
         }
 
-        let c = match self.peek_char() { Some(c) => c, None => return None };
+        let c = match self.peek_char() { Some(c) => c, None => return Ok(EOF) };
 
         match c {
-            '(' => { self.pop_char(); return Some(Ok(LBrace)); },
-            ')' => { self.pop_char(); return Some(Ok(RBrace)); },
-            '\'' => { self.pop_char(); return Some(Ok(Quote)); },
-            '`' => { self.pop_char(); return Some(Ok(QuasiQuote)); },
-            ',' => { self.pop_char(); return Some(Ok(Unquote)); },
-            '@' => { self.pop_char(); return Some(Ok(Unpack)); },
+            '(' => { self.pop_char(); return Ok(LBrace); },
+            ')' => { self.pop_char(); return Ok(RBrace); },
+            '\'' => { self.pop_char(); return Ok(Quote); },
+            '`' => { self.pop_char(); return Ok(QuasiQuote); },
+            ',' => { self.pop_char(); return Ok(Unquote); },
+            '@' => { self.pop_char(); return Ok(Unpack); },
             'a'..'z' | 'A'..'Z' | ':' | '_' => {
                 let symbol_start = self.cursor;
                 self.pop_char();
@@ -81,11 +79,9 @@ impl<'src> Iterator<TokenizerResult<'src>> for Tokenizer<'src> {
                     self.pop_char();
                 }
 
-                return Some(Ok(
-                    Symbol(self.input.slice(symbol_start, self.cursor))
-                ));
+                return Ok(Symbol(self.input.slice(symbol_start, self.cursor)));
             }
-            _ => return Some(Err(TokenizerError))
+            _ => return Err(TokenizerError)
         };
     }
 }
@@ -101,19 +97,19 @@ mod test {
     #[test]
     fn test_braces() {
         let mut tokenizer = tokenize("()(");
-        assert_eq!(tokenizer.next(), Some(Ok(LBrace)));
-        assert_eq!(tokenizer.next(), Some(Ok(RBrace)));
-        assert_eq!(tokenizer.next(), Some(Ok(LBrace)));
-        assert_eq!(tokenizer.next(), None);
+        assert_eq!(tokenizer.next_token(), Ok(LBrace));
+        assert_eq!(tokenizer.next_token(), Ok(RBrace));
+        assert_eq!(tokenizer.next_token(), Ok(LBrace));
+        assert_eq!(tokenizer.next_token(), Ok(EOF));
     }
 
     #[test]
     fn test_symbols() {
         let mut tokenizer = tokenize("aAL_-");
-        assert_eq!(tokenizer.next(), Some(Ok(Symbol("aAL_-"))));
+        assert_eq!(tokenizer.next_token(), Ok(Symbol("aAL_-")));
 
         let mut tokenizer = tokenize("one two");
-        assert_eq!(tokenizer.next(), Some(Ok(Symbol("one"))));
-        assert_eq!(tokenizer.next(), Some(Ok(Symbol("two"))));
+        assert_eq!(tokenizer.next_token(), Ok(Symbol("one")));
+        assert_eq!(tokenizer.next_token(), Ok(Symbol("two")));
     }
 }
