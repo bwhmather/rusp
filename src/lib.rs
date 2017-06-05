92d784e3f19fea69607b3b80c40fafae3b12016b
Ben Mather <bwhmather@bwhmather.com>
2017-06-05 21:03:26 +0100

    Hack together an eval that can add two numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,84 @@
 pub mod types;
 pub mod lexer;
 pub mod parser;
+
+use std::rc::Rc;
+
+use types::Expression;
+
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Value {
+    Int(i64),
+    Str(Rc<String>),
+    List(Rc<Vec<Value>>),
+}
+
+
+fn op_add(args: &[Value]) -> Value {
+    if let (&Value::Int(a), &Value::Int(b)) = (&args[0], &args[1]) {
+        return Value::Int(a + b);
+    } else {
+        panic!("missing argument");
+    }
+}
+
+
+fn eval(expr: &Expression) -> Value {
+    match expr {
+        &Expression::List(ref elements) => {
+            match elements[0] {
+                Expression::Symbol(ref name) => {
+                    match name.as_ref() {
+                        "if" => {
+                            unimplemented!();
+                        }
+                        "define" => {
+                            unimplemented!();
+                        }
+                        "+" => {
+                            let args: Vec<Value> =
+                                elements[1..].iter().map(eval).collect();
+                            op_add(&args)
+                        }
+                        _ => {
+                            unimplemented!();
+                        }
+                    }
+                }
+                _ => {
+                    unimplemented!();
+                }
+            }
+        }
+        &Expression::Int(i) => {
+            return Value::Int(i);
+
+        }
+        &Expression::Symbol(ref name) => {
+            unimplemented!();
+        }
+        _ => {
+            unimplemented!();
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use lexer::tokenize;
+    use parser::read;
+    use Value;
+    use eval;
+
+    #[test]
+    fn test_add() {
+        let expr = read(tokenize("(+ 1 1)").unwrap()).unwrap();
+
+        assert_eq!(
+            eval(&expr),
+            Value::Int(2)
+        )
+    }
+}
